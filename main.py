f49ed48547514cb141c477bbbf4c4e000487de21
Ilya Burakov <speedwatson@gmail.com>
2017-11-08 14:41:57 +0300

    index placeholder

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/login', methods=['POST'])
-def hello_world():
+@app.route('/')
+def index():
     return '<div style="text-align: center;"> <h1>HALLOU!!1 </h1></div> '
 
